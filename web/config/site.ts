99980ae6f5f0ef23499a3902e830fb5a737b803a
Coding-With-Josh <codewithjoshh@gmail.com>
2025-05-16 19:45:33 +0100

    first commit

---
+++ web/config/site.ts
@@ -0,0 +1,47 @@
+import { SidebarNavItem, SiteConfig } from "types";
+import { env } from "@/env.mjs";
+
+const site_url = env.NEXT_PUBLIC_APP_URL;
+
+export const siteConfig: SiteConfig = {
+  name: "Edo Edu",
+  description:
+    "Possess the power of centralized systems, and manage your school efficiently",
+  url: site_url,
+  ogImage: `${site_url}/_static/og.jpg`,
+  links: {
+    twitter: "https://x.com/josh_scriptz",
+    github: "https://github.com/Coding-With-Josh/edo-edu",
+  },
+  mailSupport: "codewithjoshh@gmail.com",
+};
+
+export const footerLinks: SidebarNavItem[] = [
+  {
+    title: "Company",
+    items: [
+      { title: "About", href: "#" },
+      { title: "Enterprise", href: "#" },
+      { title: "Terms", href: "/terms" },
+      { title: "Privacy", href: "/privacy" },
+    ],
+  },
+  {
+    title: "Product",
+    items: [
+      { title: "Security", href: "#" },
+      { title: "Customization", href: "#" },
+      { title: "Customers", href: "#" },
+      { title: "Changelog", href: "#" },
+    ],
+  },
+  {
+    title: "Docs",
+    items: [
+      { title: "Introduction", href: "#" },
+      { title: "Installation", href: "#" },
+      { title: "Components", href: "#" },
+      { title: "Code Blocks", href: "#" },
+    ],
+  },
+];
